31b53ff43233c3a83a1a387007312e720b2ff07b
Noah Clark <Clarknoah@live.com>
2026-06-10 14:16:18 -0500

    learning nodeJs

---
+++ NodeJs/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+import { randomSuperhero } from "superheroes";
+const name = randomSuperhero();
+
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+console.log(`hello my name is ${name}.`);
